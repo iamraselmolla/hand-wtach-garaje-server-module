c69643ffefd7e190a4aa0da79596a7576c2ec479
iamraselmolla <raselmolla6336@gmail.com>
2022-11-29 02:01:03 +0600

    added payment verify api

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const jwt = require('jsonwebtoken')
+const stripe = require("stripe")(process.env.stripeKey);
+
 require('dotenv').config();
 const app = express()
 const cors = require('cors');
@@ -11,12 +13,29 @@ const port = process.env.PORT || 5000;
 // Database connection
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.q37bxqk.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-
+console.log(process.env.stripeKey)
 async function run() {
     try {
         const usersCollection = client.db("buywatch").collection("users");
         const watchesCollection = client.db("buywatch").collection("watches");
         const categoriesCollection = client.db("buywatch").collection("categories");
+        const bookedCollection = client.db("buywatch").collection("booked");
+        // Payment method
+        app.post('/create-payment-intent', async (req, res) => {
+            const booking = req.body;
+            const amount = booking.price * 100;
+            const paymentIntent = await stripe.paymentIntents.create({
+                currency: "usd",
+                amount: amount,
+                "payment_method_types" : [
+                    "card"
+                ]
+              });
+            
+              res.send({
+                clientSecret: paymentIntent.client_secret,
+              });
+        })
         // Saving user registration data
         app.post('/users', async(req,res) => {
             const user = req.body;
@@ -123,11 +142,15 @@ async function run() {
         // Reported items
         app.put('/items/reported/:id', async (req, res) => {
             const id = req.params.id;
+            const reportedTime= req.body.reportedTime;
             const filter = {_id : ObjectId(id)};
             const options = { upsert: true };
+            console.log(req.body)
             const updatedDoc = {
                 $set: {
-                    reported: true
+                    reported: true,
+                    reportedTime: reportedTime
+
                 }
             }
             const result = await watchesCollection.updateOne(filter, updatedDoc, options);
@@ -190,7 +213,58 @@ async function run() {
             const result = await watchesCollection.find(query).limit(dataLimit).toArray();
             res.send(result)
         })
+        // Booked item
+        app.post('/booked', async (req, res) => {
+            const data = req.body;
+            const result =  await bookedCollection.insertOne(data);
+            res.send(result)
+        })
+        // find booked item
+        app.get('/booked', async (req, res) => {
+            const email = req.query.email;
+            const query = {email: email, paid:false}
+            const result =  await bookedCollection.find(query).toArray();
+            res.send(result)
+        })
+        // Find single items
+        app.get('/pay/:id', async(req, res) => {
+            const id = req.params.id;
+            const query =  {_id : ObjectId(id)};
+            const result = await watchesCollection.findOne(query);;
+            res.send(result);
+        })
+        // added payment method data to db
+        app.put('/payment-done/:id', async (req, res) => {
+            const id = req.params.id
+            const query = {product_id : id}
+            const options = { upsert: true };
+            const updatedDoc = {
+                $set: {
+                    paid: true,
+                    payment_id: req.body.payment_id,
+                    payment_type: req.body.payment_type,
+                    paymentTime: req.body.paymentTime
 
+
+                }
+            }
+            const result = await bookedCollection.updateOne(query, updatedDoc, options);
+            res.send(result)
+        })
+        // Find my shopping
+        app.get('/my-shopping', async(req, res)=> {
+            const email = req.query.email;
+            const query = {email: email, paid:true};
+            const result = await bookedCollection.find(query).toArray()
+            res.send(result)
+        })
+
+        // payment verification
+        app.get('/payment-verification', async (req, res) => {
+            const query = {payment_id: req.query.paymentID}
+            const result = await bookedCollection.findOne(query);
+            res.send(result)
+        })
 
     }
     finally {
